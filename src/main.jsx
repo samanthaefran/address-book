e981e7f692278aa1c82e23813fa4d7fe1e2a97cd
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2023-01-24 09:15:39 -0500

    creating delete function

--- src/main.jsx
+++ src/main.jsx
@@ -23,6 +23,9 @@ import EditContact, {
   action as editAction,
 }from "./routes/edit"
 
+import { action as destroyAction } from "./routes/destroy"
+
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,6 +44,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction, 
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       }
     ]
   }
